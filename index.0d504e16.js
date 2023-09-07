1b67df0da0c347d757163eee9151fa02bb767b24
johncleece <47151315+johncleece@users.noreply.github.com>
2023-09-07 10:46:10 +0000

    Deploying to gh-pages from @ johncleece/team-project@352847ff6f684d8a66475bb8a256176a2f9e420f 🚀

---
+++ index.0d504e16.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[burger-modal-open]"),closeModalBtn:document.querySelector("[burger-modal-close]"),modal:document.querySelector("[burger-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.0d504e16.js.map
