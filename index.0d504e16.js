cffc5613233f846e415618e90d006ef4bcfad395
johncleece <47151315+johncleece@users.noreply.github.com>
2023-09-11 11:16:08 +0000

    Deploying to gh-pages from @ johncleece/team-project@2569cd759ce2f9a5e1487084dd6a0bf25c3e8fea 🚀

--- index.0d504e16.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[burger-modal-open]"),closeModalBtn:document.querySelector("[burger-modal-close]"),modal:document.querySelector("[burger-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.0d504e16.js.map
--- index.b64a85e4.js
+++
@@ -1,2 +0,0 @@
-function displayImage(e){var t=document.getElementById("imagecontainer"),a=document.createElement("img");a.classList="sale-watch-preview",a.src=e,a.alt="Selected Image",a.height=350,a.width=235,t.innerHTML="",t.appendChild(a)}
-//# sourceMappingURL=index.b64a85e4.js.map
